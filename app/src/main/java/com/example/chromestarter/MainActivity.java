d057f081c9111dc7eaac108db587d60b8941d68d
ALEXDRUGS <alex.drugs1979@gmail.com>
2025-01-02 20:59:44 +0300

    update gradle

--- app/src/main/java/com/example/chromestarter/MainActivity.java
+++ app/src/main/java/com/example/chromestarter/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("QueryPermissionsNeeded")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
