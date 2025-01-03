6ce27de50b1d9936b30605d0b6a67700ab99b07f
ALEXDRUGS <alex.drugs1979@gmail.com>
2025-01-03 22:51:31 +0300

    EMPTY1

--- app/src/main/java/com/example/chromestarter/MainActivity.java
+++ app/src/main/java/com/example/chromestarter/MainActivity.java
@@ -3,10 +3,20 @@ package com.example.chromestarter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.os.Environment;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +25,96 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button button = findViewById(R.id.button);
-        button.setOnClickListener(view -> {
-            Uri address = Uri.parse("https://www.google.ru");
-            Intent open = new Intent(Intent.ACTION_VIEW, address);
-            if (open.resolveActivity(getPackageManager()) != null) {
-                startActivity(open);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.button) {
+            readFile();
+        }
+    }
+
+    final String LOG_TAG = "myLogs";
+    final String FILENAME = "/storage/sdcard0/Music";
+    final String DIR_SD = "MyFiles";
+    final String FILENAME_SD = "fileSD";
+
+    void writeFile() {
+        try {
+            // отрываем поток для записи
+            BufferedWriter bw = new BufferedWriter(
+                    new OutputStreamWriter(openFileOutput(FILENAME, MODE_PRIVATE)));
+            // пишем данные
+            bw.write("Содержимое файла");
+            // закрываем поток
+            bw.close();
+            Log.d(LOG_TAG, "Файл записан");
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+    void readFile() {
+        try {
+            // открываем поток для чтения
+            BufferedReader br = new BufferedReader(new InputStreamReader(openFileInput(FILENAME)));
+            String str;
+            // читаем содержимое
+            while ((str = br.readLine()) != null) {
+                Log.d(LOG_TAG, str);
             }
-        });
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+    void writeFileSD() {
+        // проверяем доступность SD
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Log.d(LOG_TAG, "SD-карта не доступна: " + Environment.getExternalStorageState());
+            return;
+        }
+        // получаем путь к SD
+        File sdPath = Environment.getExternalStorageDirectory();
+        // добавляем свой каталог к пути
+        sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
+        // создаем каталог
+        // формируем объект File, который содержит путь к файлу
+        File sdFile = new File(String.valueOf(sdPath.mkdirs()), FILENAME_SD);
+        try {
+            // открываем поток для записи
+            BufferedWriter bw = new BufferedWriter(new FileWriter(sdFile));
+            // пишем данные
+            bw.write("Содержимое файла на SD");
+            // закрываем поток
+            bw.close();
+            Log.d(LOG_TAG, "Файл записан на SD: " + sdFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+    void readFileSD() {
+        // проверяем доступность SD
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Log.d(LOG_TAG, "SD-карта не доступна: " + Environment.getExternalStorageState());
+            return;
+        }
+        // получаем путь к SD
+        File sdPath = Environment.getExternalStorageDirectory();
+        // добавляем свой каталог к пути
+        sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
+        // формируем объект File, который содержит путь к файлу
+        File sdFile = new File(sdPath, FILENAME_SD);
+        try {
+            // открываем поток для чтения
+            BufferedReader br = new BufferedReader(new FileReader(sdFile));
+            String str;
+            // читаем содержимое
+            while ((str = br.readLine()) != null) {
+                Log.d(LOG_TAG, str);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
